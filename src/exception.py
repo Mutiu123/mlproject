a189a1eee5c5066cb3f1509635e8f916a2e6af30
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-07-30 23:48:23 +0100

    ad Feature Engineering

--- src/exception.py
+++ src/exception.py
@@ -21,3 +21,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message 
     
+if __name__=="__main__":
+    try:
+        result = 5/0
+    except Exception as e:
+        
+        logging.info("Division by zero error")
+        raise CustomException(e, sys)
+    
