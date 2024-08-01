32ab09a11b8a7e95d454a01b2516f4d56b59be6a
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-08-01 15:02:31 +0100

    add app and Prediction pipline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -108,7 +108,7 @@ class ModelTrainer:
 
             if best_model_score<0.7:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+            logging.info(f"Found best model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
